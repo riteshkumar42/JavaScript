5c1fd0350d8d85f47d53911e28c43918100796ce
riteshkumar42 <riteshstark24970@gmail.com>
2025-06-07 22:19:11 +0530

    update...

---
+++ 01_basics/03_conversationOperation.js
@@ -0,0 +1,39 @@
+let score = "47"  // null , undefined, boolen , 34 , string
+
+console.log(typeof(score));
+
+
+let valueInNuber = Number(score)
+
+console.log(typeof(valueInNuber));
+console.log(valueInNuber)
+
+
+
+//"45" => 45
+//"33abc" => NaN  (Not a number)
+// true => 1; false => 0
+
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1 => true; 0 => false
+// "" => false
+// "ritesh" => true
+
+
+
+let someNumber = 99
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+// 99
+// string
